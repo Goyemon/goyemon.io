7f09a4a6408d93536544eb81607e3ac289056613
Tai <4382091+taisukemino@users.noreply.github.com>
2020-05-26 15:26:51 +0900

    add a privacy policy

---
+++ src/pages/privacy-policy.js
@@ -0,0 +1,142 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+import Layout from '@common/Layout';
+import { Container } from '@components/global';
+
+const PrivacyPolicy = () => (
+  <Layout>
+    <Container>
+      <br />
+      <br />
+      <p>
+        Article 1 (Basic Principles for Privacy Protection) In obtaining
+        Personal Information of customers using the Services (the “Users”) in
+        connection with the operation of certain storage and remittance services
+        of virtual currency and other related services under the name of
+        “Goyemon” (the “Services”), Swarm, K.K. (the “Company”) recognizes the
+        importance of protecting Personal Information and, in order to comply
+        with the Act on the Protection of Personal Information (the “Act”) and
+        other laws and regulations, the Company will endeavor to handle the
+        Users’ Personal Information in an appropriate manner in accordance with
+        the following privacy policy (the “Policy”).
+      </p>
+      <br />
+      <p>
+        Article 2 (Personal Information) In this Policy, “Personal Information”
+        means the personal information defined in Article 2, Paragraph 1 of the
+        Act, i.e., the information regarding a living individual that can
+        identify a specific individual by name, date of birth or other
+        description contained in such information (including information that
+        may be easily collated with other information and will thereby enable
+        identification of a specific individual).
+      </p>
+      <br />
+      <p>
+        Article 3 (Proper Acquisition of Personal Information) The Company will
+        acquire Personal Information by proper means and will not obtain
+        Personal Information by deception or other wrongful means.
+      </p>
+      <br />
+      <p>
+        Article 4 (Purpose of Use) The Company will use the Users’ Personal
+        Information for the following purposes. In addition, the Company may
+        change the purposes of use to a reasonable extent, and in the event of
+        any change, it will notify the Users or publicly announce such change.
+        (1) to provide and develop the Services; (2) to give notifications with
+        respect to the Services; (3) to improve user experience of the Services
+        and related services; (4) to give notifications with respect to various
+        promotions relating to the Services; (5) to conduct surveys, marketing
+        activities, and market searches and analyses to improve the quality of
+        the Services; (6) to disclose or provide information obtained through
+        the aggregation and analyses of information relating to the Services to
+        third parties in a manner whereby individuals are unidentifiable or
+        unspecifiable; (7) to provide maintenance and support by the Company
+        regarding the Services; (8) to respond to actions that violate the
+        terms, policies, etc. with respect to the Services (the “Terms”); (9) to
+        verify accounts at financial institutions and to confirm completion of
+        remittance; (10) to protect the Services and the Users’ and other third
+        parties’ rights, property, services, etc. when necessary; and (11) to
+        serve any other purposes incidental to the purpose of use above.
+      </p>
+      <br />
+      <p>
+        Article 5 (Provision of Personal Information to Third Parties) The
+        Company will not provide Personal Information to any third party unless
+        permitted under the Act or any other laws or regulations; provided,
+        however, the foregoing will not apply if: (1) the User consents; (2) the
+        Company outsources the handling of Personal Information in whole or in
+        part to the extent necessary to achieve the purpose of use; (3) Personal
+        Information is provided upon succession of business due to merger or
+        other events; (4) there is reasonable suspicion that a User violated the
+        terms of use and it is considered necessary to protect the rights,
+        interest, property, etc. of the Company; (5) there is danger to the
+        life, body, property, etc. of another User or a third party, and it is
+        difficult to obtain consent of the User; or (6) the disclosure of
+        Personal Information is required by a valid order issued by a public
+        organization pursuant to laws and regulations.
+      </p>
+      <br />
+      <p>
+        Article 6 (Secure Management of Personal Information) The Company will
+        provide necessary and appropriate supervision to its officers and
+        employees in order to ensure secure management against risks such as
+        alternation, loss, leakage, etc. of Personal Information. In addition,
+        in the event the Company outsources the handling of Personal Information
+        in whole or in part, the Company will provide necessary and appropriate
+        supervision to ensure secure management of Personal Information by the
+        outsourcee.
+      </p>
+      <br />
+      <p>
+        Article 7 (Disclosure of Personal Information, etc.) In the event a User
+        requests to disclose, or to correct, add or delete Personal Information
+        (“Correct”), or to discontinue the use of or erase Personal Information
+        (“Discontinue”) under the Act, the Company will, after confirming that
+        the request is made by the User, disclose to, or Correct or Discontinue
+        for the User without delay (or in the event such action will not be
+        taken pursuant to the Act, the Company will notify the User thereof);
+        provided, however, that this provision will not apply if the Company is
+        not obligated to disclose, Correct or Discontinue under the Act or other
+        laws and regulations. A fee (1,000 yen (tax inclusive) per request) will
+        be required for any request to disclose, Correct or Discontinue the use
+        of Personal Information.
+      </p>
+      <br />
+      <p>
+        Article 8 (Disclaimer) The Company will not be liable in any of the
+        following circumstances: (1) in the event a User himself/herself
+        discloses Personal Information to a third party; or (2) in the event
+        information entered directly by a User is available to a third party and
+        the User is identified by the third party due to that information.
+      </p>
+      <br />
+      <p>
+        Article 9 (Use of Cookies and Other Technologies) Cookies or similar
+        technologies may be used in the Company’s services. Such technologies
+        help the Company to recognize the status of use of the Services and
+        other matters and contribute to improvement of the Services. The Users
+        may disable cookies by changing the web browser settings. However,
+        please note that when cookies are disabled, parts of the service may
+        become unavailable.
+      </p>
+      <br />
+      <p>
+        Article 10 (Amendment of the Policy) Unless provided otherwise by laws
+        and regulations, the Company may change the Policy from time to time in
+        accordance with the procedures set by the Company.
+      </p>
+      <br />
+      <p>
+        Article 11 (Inquiry) Please contact the following for inquiries
+        regarding the handling of Personal Information:
+        Mail：tai@swarm-tokyo.com Enacted on the 1st day of June 2020.
+      </p>
+      <br />
+      <br />
+      <Link to="/">Go back to the homepage</Link>
+    </Container>
+  </Layout>
+);
+
+export default PrivacyPolicy;
